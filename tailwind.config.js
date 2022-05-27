1d6d4c5847139960a421f0ed42a879645078a8f0
Triin Trempi <tri0022@stud.kea.dk>
2022-05-27 10:59:03 +0200

    initialised repository

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#364259",
+        secondary: "#748097",
+        light: "#F8FBFD",
+        dark: "#161A24",
+        accent: "#96B58A",
+      },
+
+      fontFamily: {
+        lato: "'Lato', sans-serif",
+        shadows: "'Shadows Into Light Two', cursive",
+      },
+
+      fontSize: {
+        xs: ".75rem",
+        sm: "1rem",
+        tiny: "1.25rem",
+        base: "1.375rem",
+        lg: "1.625rem",
+        xl: "2rem",
+        "2xl": "3rem",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("daisyui")],
+};
