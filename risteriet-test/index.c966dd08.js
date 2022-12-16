3110693a267aa7aef26c2f3a5c77b2e7bd5a48d6
Mosholm <Oliv387u@stud.kea.dk>
2022-12-16 01:08:32 +0100

    init

--- risteriet-test/index.c966dd08.js
+++
@@ -1,2 +0,0 @@
-gsap.registerPlugin(ScrollTrigger,ScrollSmoother);let smoother=scrollSmoother.create({smooth:2,smoothTouch:.1,effects:!0});ScrollTrigger.create({trigger:"#section3",pin:!0,start:"center center",end:"+=300"});
-//# sourceMappingURL=index.c966dd08.js.map
